d237f8b36e7265089e5faab88d0bbb97b9a7743f
ZGZLCC <1075300572@qq.com>
2025-12-18 21:06:14 +0800

    feat(config): 更新站点配置与个人信息
    将站点背景图片、favicon 和用户链接等配置项的注释翻译为中文，并更新了个人资料相关配置。
    同时修改了 Footer 组件中的备案信息显示逻辑，将其注释掉。新增 zgzlcc.png 资源文件。

--- src/config.ts
+++ src/config.ts
@@ -9,70 +9,70 @@ export const siteConfig: SiteConfig = {
     subTitle: "记录生活",
 
     backgroundImage: {
-        enabled: false, // If backgroundImage is not enabled, this will be used as the background color
-        url: "assets/background.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+        enabled: false, // 如果未启用背景图像，则这将用作背景颜色
+        url: "assets/background.png", // 相对于 /src 目录。如果以 '/' 开头则相对于 /public 目录
     },
 
-    favicon: "/favicon/favicon.ico" // Path of the favicon, relative to the /public directory
+    favicon: "/favicon/favicon.ico" // favicon 的路径，相对于 /public 目录
 }
 
 export const profileConfig: ProfileConfig = {
-    avatar: "assets/Motues.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    name: "Motues",
-    description: "Like River!",
-    introPage: "https://motues.top",
+    avatar: "assets/zgzlcc.png", // 相对于 /src 目录。如果以 '/' 开头则相对于 /public 目录
+    name: "ZGZLCC",
+    description: "",
+    introPage: "https://zgzlcc.github.io",
     links: [
-        /* -------------------------------- Icon -------------------------------- */
-        // Visit https://icones.js.org/ for icon codes
-        // You will need to install the corresponding icon set if it's not already included
+        /* -------------------------------- 图标 -------------------------------- */
+        // 访问 https://icones.js.org/ 获取图标代码
+        // 如果尚未包含相应的图标集，您需要安装它
         // `pnpm add @iconify-json/<icon-set-name>`
-        // Then add icon to defineConfig.integrationsin.icon in astro.config.mjs
-        // You can also use local svg icons end with .svg, relative to the src directory
-        // eg. `icon: "assets/icons/github.svg"`
-        /* -------------------------------- Color -------------------------------- */
-        // The colors a best to be clearly visible in both light and dark environments 
-        // If color is not set, it will use the icon's default color
+        // 然后在 astro.config.mjs 的 defineConfig.integrationsin.icon 中添加图标
+        // 您也可以使用以 .svg 结尾的本地 svg 图标，相对于 src 目录
+        // 例如 `icon: "assets/icons/github.svg"`
+        /* -------------------------------- 颜色 -------------------------------- */
+        // 颜色最好在明亮和黑暗环境中都能清晰可见
+        // 如果未设置颜色，将使用图标的默认颜色
         {
-          name: "Blog",
-          url: "https://blog.motues.top",
-          icon: "fa6-solid:tree", 
-          color: "#0dbc79",
+          name: "miniprogram",
+          url: "https://zgzlcc.github.io/miniprogram",
+          icon: "fa6-solid:heart", 
+          color: "#db0f0fff",
         },
-        {
-            name: "Note",
-            url: "https://note.motues.top",
-            icon: "fa6-solid:book",
-            color: "#b4a992",
-        },
-        {
-            name: "Lab",
-            url: "/lab",
-            icon: "fa6-solid:server",
-            color: "#72b6b3",
-        },
+        // {
+        //     name: "Note",
+        //     url: "https://note.motues.top",
+        //     icon: "fa6-solid:book",
+        //     color: "#b4a992",
+        // },
+        // {
+        //     name: "Lab",
+        //     url: "/lab",
+        //     icon: "fa6-solid:server",
+        //     color: "#72b6b3",
+        // },
         {
             name: "GitHub",
-            url: "https://github.com/Motues",
+            url: "https://github.com/ZGZLCC",
             icon: "fa6-brands:github",
             color: "#888",
         },
-        {
-            name: "Netease",
-            url: "https://music.163.com/#/user/home?id=1601379853",
-            icon: "simple-icons:neteasecloudmusic",
-            color: "#ff2822",
-        },
-        {
-            name: "Mail",
-            url: "mailto:me@motues.top",
-            icon: "fa6-solid:envelope",
-            color: "#43a4d1ff"
-        },
+        // {
+        //     name: "Netease",
+        //     url: "https://music.163.com/#/user/home?id=1601379853",
+        //     icon: "simple-icons:neteasecloudmusic",
+        //     color: "#ff2822",
+        // },
+        // {
+        //     name: "Mail",
+        //     url: "mailto:me@motues.top",
+        //     icon: "fa6-solid:envelope",
+        //     color: "#43a4d1ff"
+        // },
     ]
 }
 
 export const labConfig : LabConfig = [
-    // Icon and color rules are same as profileConfig
+    // 图标和颜色规则与 profileConfig 相同
     {
         name: "BlockBoard",
         description: "A real-time online block board",
